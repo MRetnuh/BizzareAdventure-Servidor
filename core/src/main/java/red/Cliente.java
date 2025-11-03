cd986875c18f940c508b5594e3bdbd9822570bc6
MrHunter <eduardoorsi2006@gmail.com>
2025-11-03 14:32:59 -0300

    ayuda

--- core/src/main/java/red/Cliente.java
+++ core/src/main/java/red/Cliente.java
@@ -1,5 +1,34 @@
 package red;
 
+import java.net.InetAddress;
+
 public class Cliente {
 
+    private String id;
+    private int num;
+    private InetAddress ip;
+    private int port;
+
+    public Cliente(int num, InetAddress ip, int port) {
+        this.num = num;
+        this.id = ip.toString() + ":" + port;
+        this.ip = ip;
+        this.port = port;
+    }
+
+    public String getId() {
+        return this.id;
+    }
+
+    public InetAddress getIp() {
+        return this.ip;
+    }
+
+    public int getPort() {
+        return this.port;
+    }
+
+    public int getNum() {
+        return this.num;
+    }
 }
--- core/src/main/java/red/HiloServidor.java
+++ core/src/main/java/red/HiloServidor.java
@@ -1,5 +1,37 @@
 package red;
 
+import java.io.IOException;
+import java.net.DatagramPacket;
+import java.net.DatagramSocket;
+import java.net.SocketException;
+import java.util.ArrayList;
+
 public class HiloServidor extends Thread{
+	
+	private DatagramSocket socket;
+    private int servidorPort = 5555;
+    private boolean fin = false;
+    private final int MAX_CLIENTES = 2;
+    private int clientesConectados = 0;
+    private ArrayList<Cliente> clientes = new ArrayList<Cliente>();
+    
+    public HiloServidor() {
+        try {
+            this.socket = new DatagramSocket(this.servidorPort);
+        } catch (SocketException e) {
+//            throw new RuntimeException(e);
+        }
+    }
+    @Override
+    public void run() {
+        do {
+            DatagramPacket paquete = new DatagramPacket(new byte[1024], 1024);
+            try {
+                this.socket.receive(paquete);
+            } catch (IOException e) {
+//                throw new RuntimeException(e);
+            }
+        } while(!this.fin);
+    }
 
 }
