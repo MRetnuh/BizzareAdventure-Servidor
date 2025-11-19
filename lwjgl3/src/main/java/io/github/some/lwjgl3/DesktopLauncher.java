5b1048c5fe5adb400108455a768190083bd4b2d1
MrHunter <eduardoorsi2006@gmail.com>
2025-11-19 17:50:20 -0300

    desktop arreglado (creo)

--- lwjgl3/src/main/java/io/github/some/lwjgl3/DesktopLauncher.java
+++ lwjgl3/src/main/java/io/github/some/lwjgl3/DesktopLauncher.java
@@ -1,17 +1,21 @@
 package io.github.some.lwjgl3;
 
+import com.badlogic.gdx.Graphics;
 import com.badlogic.gdx.backends.lwjgl3.Lwjgl3Application;
 import com.badlogic.gdx.backends.lwjgl3.Lwjgl3ApplicationConfiguration;
-import io.github.some.Principal; // Tu clase principal en core
+import io.github.some.Principal;
 
 public class DesktopLauncher {
     public static void main(String[] args) {
         Lwjgl3ApplicationConfiguration config = new Lwjgl3ApplicationConfiguration();
 
-        // Ejecutar en pantalla completa
-        config.setFullscreenMode(Lwjgl3ApplicationConfiguration.getDisplayMode());
+        // Fullscreen borderless sin usar 'var'
+        Graphics.DisplayMode display = Lwjgl3ApplicationConfiguration.getDisplayMode();
+        config.setWindowedMode(display.width, display.height);
+        config.setResizable(false);
 
-        config.setTitle("Mi Juego");
+        config.setTitle("BizzareAdventure (Servidor)");
+        config.useVsync(true);
 
         new Lwjgl3Application(new Principal(), config);
     }
