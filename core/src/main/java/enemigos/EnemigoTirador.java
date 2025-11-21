96337615fc4cd1684e3f7ff21246c32027b2deac
MejiaEynar <eynarmejia03@gmail.com>
2025-11-21 16:39:35 -0300

    Correccion balas

--- core/src/main/java/enemigos/EnemigoTirador.java
+++ core/src/main/java/enemigos/EnemigoTirador.java
@@ -70,6 +70,7 @@ public class EnemigoTirador extends EnemigoBase {
         while (it.hasNext()) {
             Proyectil b = it.next();
             b.mover(delta, nivel, this);
+            hiloServidor.sendMessageToAll("ActualizarBalas:" + super.getNombre() + "," + b.getX() + "," + b.getY() + ","+ super.getRutaBala());
             if (!b.isActivo()) {
             	it.remove(); 
             	hiloServidor.sendMessageToAll(String.format("BalaImpactada:%s", super.getNombre()));
@@ -97,8 +98,8 @@ public class EnemigoTirador extends EnemigoBase {
     private void disparar(String ruta, float volumen, HiloServidor hiloServidor) {
     	Proyectil b = new Proyectil(getX(), getY() + 16, super.moviendoDerecha, ruta);
         super.balas.add(b);
-        hiloServidor.sendMessageToAll("BalasEnemigos:" + super.getNombre() + "," + b.getX() + "," + b.getY() + ","+ super.getRutaBala());
         EfectoSonido.reproducir(super.nombreAtaque, volumen);
+        hiloServidor.sendMessageToAll("BalasEnemigos:" + super.getNombre() + "," + b.getX() + "," + b.getY() + ","+ super.getRutaBala());
         
     }
 
